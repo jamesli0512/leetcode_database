bfdd5be09a17659540e551f8866b8667c6eda751
Haotong(James) Li <47588270+jamesli0512@users.noreply.github.com>
2019-07-15 13:40:24 -0700

    Update and rename 175. Combine_Two_Tables to 175. Combine_Two_Tables.sql

--- 175. Combine_Two_Tables.sql
+++ 175. Combine_Two_Tables.sql
@@ -21,6 +21,5 @@ Table: Address
 AddressId is the primary key column for this table.
  
 
-Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
-
-FirstName, LastName, City, State
+Write a SQL query for a report that provides the following information for each person in the Person table, 
+regardless if there is an address for each of those people: FirstName, LastName, City, State
