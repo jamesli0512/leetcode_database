4f0a5955042cf0ad67d451569c78c9c4cb244300
Haotong(James) Li <47588270+jamesli0512@users.noreply.github.com>
2019-07-15 22:13:09 -0700

    Update 177. Nth_Highest_Salary.sql

--- 177. Nth_Highest_Salary.sql
+++ 177. Nth_Highest_Salary.sql
@@ -8,13 +8,15 @@ Write a SQL query to get the nth highest salary from the Employee table.
 | 2  | 200    |
 | 3  | 300    |
 +----+--------+
-For example, given the above Employee table, the nth highest salary where n = 2 is 200. If there is no nth highest salary, then the query should return null.
+For example, given the above Employee table, the nth highest salary where n = 2 is 200. 
+If there is no nth highest salary, then the query should return null.
 
 +------------------------+
 | getNthHighestSalary(2) |
 +------------------------+
 | 200                    |
 +------------------------+
+
 */
 
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
@@ -25,6 +27,7 @@ SET N=N-1;
       SELECT DISTINCT Salary
       FROM Employee
       ORDER BY Salary DESC
-      LIMIT 1 OFFSET N
+      LIMIT 1 
+      OFFSET N
   );
 END
