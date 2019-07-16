c2e0428bf8120f6dc260ca54bc8ee9f376f3125a
Haotong(James) Li <47588270+jamesli0512@users.noreply.github.com>
2019-07-15 22:22:43 -0700

    Update 177. Nth_Highest_Salary.sql

--- 177. Nth_Highest_Salary.sql
+++ 177. Nth_Highest_Salary.sql
@@ -24,10 +24,10 @@ BEGIN
 SET N=N-1;
   RETURN (
       # Write your MySQL query statement below.
-      SELECT DISTINCT Salary
-      FROM Employee
-      ORDER BY Salary DESC
-      LIMIT 1 
-      OFFSET N
+      SELECT IFNULL(
+          (SELECT DISTINCT Salary
+          FROM Employee
+          ORDER BY Salary DESC
+          LIMIT 1 OFFSET N), NULL)
   );
 END
