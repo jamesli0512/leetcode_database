4d7683bfbbc4af414449384fb061481abbacad8b
Haotong(James) Li <47588270+jamesli0512@users.noreply.github.com>
2019-07-17 19:49:27 -0700

    Update 181. Employees_Earning_More_Than_Their_Managers.sql

--- 181. Employees_Earning_More_Than_Their_Managers.sql
+++ 181. Employees_Earning_More_Than_Their_Managers.sql
@@ -24,4 +24,4 @@ For the above table, Joe is the only employee who earns more than his manager.
 
 SELECT e1.Name AS Employee
 FROM Employee e1, Employee e2 
-WHERE e1.ManagerId = e2.Id AND e1.Salary > e2.Salary
+WHERE e1.ManagerId = e2.Id AND e1.Salary > e2.Salary;
