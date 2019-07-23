f0f0c9fa70523ff4c6be40eb51008fafe6ef4a98
Haotong(James) Li <47588270+jamesli0512@users.noreply.github.com>
2019-07-23 12:09:15 -0700

    Update 618. Students_Report_By_Geography.sql

--- 618. Students_Report_By_Geography.sql
+++ 618. Students_Report_By_Geography.sql
@@ -66,3 +66,18 @@ LEFT JOIN
 ON 
     t1.amid = t3.euid
 ;
+/* For the follow-up question:*/
+
+SELECT MAX(America) AS 'America', MAX(Asia) AS 'Asia', MAX(Europe) AS 'Europe'
+FROM (
+    SELECT 
+        CASE WHEN continent = 'America' THEN @r1 := @r1 + 1
+             WHEN continent = 'Asia'    THEN @r2 := @r2 + 1
+             WHEN continent = 'Europe'  THEN @r3 := @r3 + 1 END AS 'row_id',
+        CASE WHEN continent = 'America' THEN name END AS 'America',
+        CASE WHEN continent = 'Asia'    THEN name END AS 'Asia',
+        CASE WHEN continent = 'Europe'  THEN name END AS 'Europe'
+    FROM student, (SELECT @r1 := 0, @r2 := 0, @r3 := 0) AS row_id
+    ORDER BY name
+) t
+GROUP BY row_id;
